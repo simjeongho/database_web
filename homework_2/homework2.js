df9b34729521e7925d31b7a898fdac047ae60f65
simjeongho012@gmail.com <simjeongho012@gmail.com>
2021-09-13 19:33:18 +0900

    서버 구축

---
+++ homework_2/homework2.js
@@ -0,0 +1,7 @@
+const choosenumber = getElementbyId("js-guess");
+const guessnumber = getElementbyId("js-result");
+
+
+const handleGuess (e) => {
+    const display = guessnumber.querySelector("span");
+}
